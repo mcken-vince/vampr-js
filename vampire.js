9448a66cbfe6bed66bae1e9dbe3455e023093313
Vincent McKen <mcken.vince@gmail.com>
2021-08-06 15:03:07 +0000

    completed vampireWithName

--- vampire.js
+++ vampire.js
@@ -10,29 +10,50 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let numberFromOriginal = 0;
+    let currentVampire = this;
+    while(currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberFromOriginal++;
+    }
+    return numberFromOriginal;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   /** Tree traversal methods **/
 
   // Returns the vampire object with that name, or null if no vampire exists with that name
   vampireWithName(name) {
-    
+    if (this.name === name) {
+      return this;
+    }
+    if (this.offspring)
+      for (const child of this.offspring) {
+        const result = child.vampireWithName(name);
+        if (result) {
+          return result;
+        }
+      }
+    return null;
   }
 
   // Returns the total number of vampires that exist
